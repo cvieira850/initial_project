cfb6e784393ce27d3de9876e4442242a474654d1
Caio Vieira <contato@caiovieira.com.br>
2022-04-05 09:05:24 -0300

    feat: ensure PgConnection is a singleton

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,10 +1,13 @@
 import './config/module-alias'
-import { app } from '@/main/config/app'
+
 import { env } from '@/main/config/env'
 
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
 
 createConnection()
-  .then(() => app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`)))
+  .then(async () => {
+    const { app } = await import('@/main/config/app')
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  })
   .catch(console.error)
