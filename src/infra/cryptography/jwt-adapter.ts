b16334536299879a8d53b604805abb62d700e0b5
Caio Vieira <contato@caiovieira.com.br>
2022-04-18 15:42:24 -0300

    feat: ensure BcryptAdapter calls HashComparer with correct values
    issue: #8

--- src/infra/cryptography/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter.ts
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken'
 
 export class JwtAdapter implements Encrypt {
   constructor (private readonly secret: string) {}
+
   async encrypt (params: Encrypt.Params): Promise<Encrypt.Result> {
     return await jwt.sign({ id: params.plaintext }, this.secret)
   }
