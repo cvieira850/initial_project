043c1ed9e778086cdc99695b1076011e0f1e1c1d
Caio Vieira <contato@caiovieira.com.br>
2022-04-18 11:22:31 -0300

    feat: ensure AuthenticationService calls Encrypt once
    issues: #6 and #7

--- src/data/services/account/authentication/authentication.ts
+++ src/data/services/account/authentication/authentication.ts
@@ -1,15 +1,19 @@
-import { Authentication, LoadAccountByEmailRepository, HashComparer } from './authentication-protocols'
+import { Authentication, LoadAccountByEmailRepository, HashComparer, Encrypt } from './authentication-protocols'
 
 export class AuthenticationService implements Authentication {
   constructor (
     private readonly accountRepo: LoadAccountByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly encrypt: Encrypt
   ) {}
 
   async perform (params: Authentication.Params): Promise<Authentication.Result> {
     const account = await this.accountRepo.loadByEmail({ email: params.email })
     if (account !== undefined) {
-      await this.hashComparer.compare({ plaintext: params.password, digest: account.password })
+      const isValid = await this.hashComparer.compare({ plaintext: params.password, digest: account.password })
+      if (isValid) {
+        await this.encrypt.encrypt({ plaintext: account.id })
+      }
     }
     return undefined
   }
