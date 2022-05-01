5131d47145aa97432020eb134c39793832c36e50
Caio Vieira <contato@caiovieira.com.br>
2022-05-01 15:48:57 -0300

    test: ensure LoadAccountByTokenService call LoadAccountByTokenRepository with correct values
    issue: #28

--- tests/data/services/account/load-account-by-token/load-account-by-token.spec.ts
+++ tests/data/services/account/load-account-by-token/load-account-by-token.spec.ts
@@ -1,4 +1,5 @@
 import { Decrypter } from '@/data/protocols/cryptography'
+import { LoadAccountByTokenRepository } from '@/data/protocols/db'
 import { LoadAccountByTokenService } from '@/data/services'
 
 import { mock, MockProxy } from 'jest-mock-extended'
@@ -6,6 +7,7 @@ import { mock, MockProxy } from 'jest-mock-extended'
 describe('LoadAccountByTokenService', () => {
   let sut: LoadAccountByTokenService
   let decripter: MockProxy<Decrypter>
+  let loadAccountByTokenRepository: MockProxy<LoadAccountByTokenRepository>
   let accessToken: string
   let role: string
   let result: string
@@ -16,9 +18,18 @@ describe('LoadAccountByTokenService', () => {
     result = 'decrypted_id'
     decripter = mock()
     decripter.decrypt.mockResolvedValue(result)
+    loadAccountByTokenRepository = mock()
+    loadAccountByTokenRepository.loadByToken.mockResolvedValue({
+      id: 'any_id',
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password',
+      role: 'any_role',
+      access_token: 'any_token'
+    })
   })
   beforeEach(() => {
-    sut = new LoadAccountByTokenService(decripter)
+    sut = new LoadAccountByTokenService(decripter, loadAccountByTokenRepository)
   })
   describe('Decrypter', () => {
     it('Should call Decrypter with correct accessToken', async () => {
@@ -34,6 +45,15 @@ describe('LoadAccountByTokenService', () => {
       const promise = sut.perform({ accessToken, role })
 
       await expect(promise).rejects.toThrow(new Error())
+    })
+  })
+
+  describe('LoadAccountByTokenRepository', () => {
+    it('Should call LoadAccountByTokenRepository with correct values', async () => {
+      await sut.perform({ accessToken, role })
+
+      expect(loadAccountByTokenRepository.loadByToken).toHaveBeenCalledWith({ accessToken, role })
+      expect(loadAccountByTokenRepository.loadByToken).toHaveBeenCalledTimes(1)
     })
   })
 })
