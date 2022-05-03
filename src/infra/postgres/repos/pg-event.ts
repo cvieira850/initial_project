54659fc815de14a73d43e9160dad98f517da32a7
Caio Vieira <contato@caiovieira.com.br>
2022-05-03 16:55:03 -0300

    feat: ensure PgEventRepository returns an event on add success
    issue: #38

--- src/infra/postgres/repos/pg-event.ts
+++ src/infra/postgres/repos/pg-event.ts
@@ -1,9 +1,10 @@
-import { LoadEventByNameRepository } from "@/data/protocols/db";
+import { AddEventRepository, LoadEventByNameRepository } from "@/data/protocols/db";
 import { Event } from "@/infra/postgres/entities";
 import { PgRepository } from "./repository";
 
 export class PgEventRepository extends PgRepository implements
-  LoadEventByNameRepository
+  LoadEventByNameRepository,
+  AddEventRepository
 {
   async loadByName (params: LoadEventByNameRepository.Params): Promise<LoadEventByNameRepository.Result> {
     const eventRepo =  this.getRepository(Event)
@@ -14,6 +15,24 @@ export class PgEventRepository extends PgRepository implements
         name: event.name,
         user_id: event.user_id,
         description: event.description,
+        created_at: event.created_at
+      }
+    }
+  }
+
+  async add (params: AddEventRepository.Params): Promise<AddEventRepository.Result>{
+    const eventRepo = this.getRepository(Event)
+    const event = await eventRepo.save({
+      name: params.name,
+      description: params.description,
+      user_id: params.userId
+    })
+    if(event) {
+      return {
+        id: event.id.toString(),
+        name: event.name,
+        description: event.description,
+        user_id: event.user_id,
         created_at: event.created_at
       }
     }
