c0fe1e5649d1d88d295169dfa2639b0926a877fd
Caio Vieira <contato@caiovieira.com.br>
2022-05-03 10:53:30 -0300

    test: ensure MeController call LoadAccountById with correct values
    issue: #19

---
+++ tests/application/controllers/account/me/me.spec.ts
@@ -0,0 +1,52 @@
+import { MeController } from '@/application/controllers'
+import { RequiredStringValidator, StringValidator } from '@/application/validation'
+import { LoadAccountById } from '@/domain/usecases'
+
+import { mock, MockProxy } from 'jest-mock-extended'
+
+describe('Me Controller', () => {
+  let sut: MeController
+  let loadAccountById: MockProxy<LoadAccountById>
+  let accountId: string
+  let name: string
+  let email: string
+  let role: string
+  let accessToken: string
+
+  beforeAll(() => {
+    accountId = 'any_id'
+    name = 'any_name'
+    email = 'any_email'
+    role = 'any_role'
+    accessToken = 'any_token'
+    loadAccountById = mock()
+    loadAccountById.perform.mockResolvedValue({
+      id: accountId,
+      name,
+      email,
+      role,
+      access_token: accessToken
+    })
+  })
+  beforeEach(() => {
+    sut = new MeController(loadAccountById)
+  })
+
+  it('Should build Validators correctly', async () => {
+    const validators = sut.buildValidators({ headers: { accountId: 'any_id' } })
+
+    expect(validators).toEqual([
+      new RequiredStringValidator(accountId, 'accountId'),
+      new StringValidator(accountId, 'accountId')
+    ])
+  })
+
+  describe('LoadById', () => {
+    it('Should call LoadAccountById with correct values', async () => {
+      await sut.handle({ headers: { accountId } })
+
+      expect(loadAccountById.perform).toHaveBeenCalledWith({ id: accountId })
+      expect(loadAccountById.perform).toHaveBeenCalledTimes(1)
+    })
+  })
+})
