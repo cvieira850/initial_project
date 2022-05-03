da5fd4a82d965003e11921dafcc733c6ee7126af
Caio Vieira <contato@caiovieira.com.br>
2022-05-03 19:32:45 -0300

    test: ensure AddEventController calls AddEvent with correct values
    issue: #37

--- tests/application/controllers/event/add-event/add-event.spec.ts
+++ tests/application/controllers/event/add-event/add-event.spec.ts
@@ -1,19 +1,34 @@
 import { Controller, AddEventController } from '@/application/controllers'
+import { AddEvent } from '@/domain/usecases'
 import { RequiredStringValidator, StringValidator } from '@/application/validation'
+
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('AddEventController', () => {
   let sut: AddEventController
+  let addEvent: MockProxy<AddEvent>
   let accountId: string
   let name: string
   let description: string
+  let id: string
 
   beforeAll(() => {
+    id = 'any_id'
     accountId = 'any_id'
     name = 'any_name'
     description = 'any_description'
+    addEvent = mock()
+    addEvent.perform.mockResolvedValue({
+      id,
+      name,
+      description,
+      user_id: accountId,
+      created_at: new Date()
+    })
   })
+
   beforeEach(() => {
-    sut = new AddEventController()
+    sut = new AddEventController(addEvent)
   })
 
   it('Should be an instance of Controller', () => {
@@ -34,5 +49,17 @@ describe('AddEventController', () => {
       new RequiredStringValidator(description, 'description'),
       new StringValidator(description, 'description')
     ])
+  })
+
+  describe('AddEvent', () => {
+    it('Should call AddEvent with correct values', async () => {
+      await sut.handle({ headers: { accountId }, body: { name, description } })
+
+      expect(addEvent.perform).toHaveBeenCalledWith({
+        userId: accountId,
+        name,
+        description
+      })
+    })
   })
 })
