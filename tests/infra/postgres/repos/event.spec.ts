50328327bd2ef32f6eb87637a86370b92377715c
Caio Vieira <contato@caiovieira.com.br>
2022-05-03 16:55:40 -0300

    test: ensure PgEventRepository returns an event on add success
    issue: #38

--- tests/infra/postgres/repos/event.spec.ts
+++ tests/infra/postgres/repos/event.spec.ts
@@ -1,10 +1,11 @@
-import { IBackup } from "pg-mem"
-import { Repository } from "typeorm"
 import { PgEventRepository } from "@/infra/postgres/repos"
 import { Event, User } from "@/infra/postgres/entities"
 import { PgConnection } from "@/infra/postgres/helpers"
 import { makeFakeDb } from "../mocks"
 import { PgRepository } from "@/infra/postgres/repos/repository"
+
+import { IBackup } from "pg-mem"
+import { Repository } from "typeorm"
 
 describe('PgEventRepository', () => {
     let sut: PgEventRepository
@@ -46,6 +47,16 @@ describe('PgEventRepository', () => {
       const event = await sut.loadByName({ userId: '1', name: 'any_name' })
 
       expect(event).toBeUndefined()
+    })
+  })
+
+  describe('AddEventRepository', () => {
+    it('Should return an event on add success', async () => {
+      await pgUserRepo.save({ email: 'any_email', name: 'any_name', password: '1234', role: 'user' })
+
+      const event = await sut.add({ name: 'any_name', description: 'any_description', userId: '1' })
+
+      expect(event).toEqual({ id: '1', name: 'any_name', user_id: '1', description: 'any_description', created_at: expect.any(Date) })
     })
   })
 })
