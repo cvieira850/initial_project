a1a7676c7ef732ae2818b9b1436c818c57975006
Caio Vieira <contato@caiovieira.com.br>
2022-05-03 11:04:53 -0300

    feat: ensure MeController return 200 on LoadAccountById succeeds
    issue: #19

--- src/application/controllers/account/me/me.ts
+++ src/application/controllers/account/me/me.ts
@@ -1,16 +1,32 @@
 import { Controller } from '@/application/controllers'
-import { HttpRequest, HttpResponse, noContent } from '@/application/helpers'
+import { HttpRequest, HttpResponse, noContent, ok } from '@/application/helpers'
 import { ValidationBuilder as Builder, Validator } from '@/application/validation'
 import { LoadAccountById } from '@/domain/usecases'
 
+type Model = Error | {
+  id: string
+  name: string
+  email: string
+  role: string | undefined
+  accessToken: string | undefined
+}
 export class MeController extends Controller {
   constructor (private readonly loadAccountById: LoadAccountById) {
     super()
   }
 
-  async perform (httpRequest: HttpRequest): Promise<HttpResponse<any>> {
-    await this.loadAccountById.perform({ id: httpRequest.headers.accountId })
-    return noContent()
+  async perform (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
+    const user = await this.loadAccountById.perform({ id: httpRequest.headers.accountId })
+    if (!user) {
+      return noContent()
+    }
+    return ok({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      accessToken: user.access_token
+    })
   }
 
   override buildValidators (httpRequest: HttpRequest): Validator[] {
