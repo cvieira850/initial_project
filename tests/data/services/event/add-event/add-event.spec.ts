90caba5946014dfadca71f8e5af124de5dc11d32
Caio Vieira <contato@caiovieira.com.br>
2022-05-03 15:26:29 -0300

    test: ensure AddEventService return undefined if LoadEventByNameRepository returns an event
    issue: #39

--- tests/data/services/event/add-event/add-event.spec.ts
+++ tests/data/services/event/add-event/add-event.spec.ts
@@ -6,12 +6,15 @@ import { mock, MockProxy } from 'jest-mock-extended'
 describe('AddEventService', () => {
   let sut: AddEventService
   let eventRepo: MockProxy<LoadEventByNameRepository>
+  let id: string
   let name: string
   let userId: string
   let description: string
 
   beforeAll(() => {
+    id = 'any_id'
     name = 'any_name'
+    description = 'any_description'
     userId = 'any_user_id'
     eventRepo = mock()
     eventRepo.loadByName.mockResolvedValue(undefined)
@@ -27,6 +30,20 @@ describe('AddEventService', () => {
 
       expect(eventRepo.loadByName).toHaveBeenCalledWith({ name, userId })
       expect(eventRepo.loadByName).toHaveBeenCalledTimes(1)
+    })
+
+    it('Should return undefined if LoadEventByNameRepository returns an event', async () => {
+      eventRepo.loadByName.mockResolvedValueOnce({
+        id,
+        name,
+        description,
+        user_id: userId,
+        created_at: new Date()
+      })
+
+      const result = await sut.perform({ name, userId, description })
+
+      expect(result).toBeUndefined()
     })
   })
 })
