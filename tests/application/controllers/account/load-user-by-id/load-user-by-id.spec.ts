7c5c9ea2f016a27c05a00eed1f1f667b2c4310f7
Caio Vieira <contato@caiovieira.com.br>
2022-05-04 20:47:56 -0300

    test: ensure LoadUserByIdController build Validators correctle
    issue: #45

--- tests/application/controllers/account/load-user-by-id/load-user-by-id.spec.ts
+++ tests/application/controllers/account/load-user-by-id/load-user-by-id.spec.ts
@@ -1,12 +1,28 @@
 import { Controller, LoadUserByIdController } from '@/application/controllers'
+import { RequiredStringValidator, StringValidator } from '@/application/validation'
 
 describe('LoadUserByIdController', () => {
   let sut: LoadUserByIdController
+  let userId: string
+
+  beforeAll(() => {
+    userId = 'any_id'
+  })
 
   beforeEach(() => {
     sut = new LoadUserByIdController()
   })
+
   it('Should be an instance of Controller', () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('Should build Validators correctly', async () => {
+    const validators = sut.buildValidators({ params: { userId: 'any_id' } })
+
+    expect(validators).toEqual([
+      new RequiredStringValidator(userId, 'userId'),
+      new StringValidator(userId, 'userId')
+    ])
   })
 })
