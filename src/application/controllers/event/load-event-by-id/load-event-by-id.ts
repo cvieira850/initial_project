a2d64c17b8db1b46edcb7e50931065b3a29ed639
Caio Vieira <contato@caiovieira.com.br>
2022-05-06 20:32:48 -0300

    feat: ensure LoadEventByIdController calls LoadEventById usecase with correct id
    issue: #48

--- src/application/controllers/event/load-event-by-id/load-event-by-id.ts
+++ src/application/controllers/event/load-event-by-id/load-event-by-id.ts
@@ -1,9 +1,15 @@
 import { Controller } from '@/application/controllers'
 import { HttpRequest, HttpResponse } from '@/application/helpers'
 import { ValidationBuilder as Builder, Validator } from '@/application/validation'
+import { LoadEventById } from '@/domain/usecases'
 
 export class LoadEventByIdController extends Controller {
+  constructor (private readonly loadEventById: LoadEventById) {
+    super()
+  }
+
   async perform (httpRequest: HttpRequest): Promise<HttpResponse<any>> {
+    await this.loadEventById.perform({ id: httpRequest.params.eventId })
     throw new Error('Method not implemented.')
   }
 
