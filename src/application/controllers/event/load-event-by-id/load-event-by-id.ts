e0b59fad27f74cc2250982e87e6cb70027666847
Caio Vieira <contato@caiovieira.com.br>
2022-05-06 19:39:34 -0300

    feat: ensure LoadEventByIdService build validators correctly
    issue: #48

--- src/application/controllers/event/load-event-by-id/load-event-by-id.ts
+++ src/application/controllers/event/load-event-by-id/load-event-by-id.ts
@@ -1,8 +1,15 @@
 import { Controller } from '@/application/controllers'
 import { HttpRequest, HttpResponse } from '@/application/helpers'
+import { ValidationBuilder as Builder, Validator } from '@/application/validation'
 
 export class LoadEventByIdController extends Controller {
   async perform (httpRequest: HttpRequest): Promise<HttpResponse<any>> {
     throw new Error('Method not implemented.')
+  }
+
+  override buildValidators (httpRequest: HttpRequest): Validator[] {
+    return [
+      ...Builder.of({ value: httpRequest.params.eventId, fieldName: 'eventId' }).required().string().build()
+    ]
   }
 }
