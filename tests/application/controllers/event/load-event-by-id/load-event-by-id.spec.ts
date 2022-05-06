c1af1db4012c732cdf0850506e0032cdc39ce87d
Caio Vieira <contato@caiovieira.com.br>
2022-05-06 19:40:25 -0300

    test: ensure LoadEventByIdService build validators correctly
    issue: #48

--- tests/application/controllers/event/load-event-by-id/load-event-by-id.spec.ts
+++ tests/application/controllers/event/load-event-by-id/load-event-by-id.spec.ts
@@ -1,12 +1,29 @@
 import { LoadEventByIdController, Controller } from '@/application/controllers'
+import { RequiredStringValidator, StringValidator } from '@/application/validation'
 
 describe('LoadEventByIdController', () => {
   let sut: LoadEventByIdController
+  let eventId: string
+
+  beforeAll(() => {
+    eventId = 'any_id'
+  })
 
   beforeEach(() => {
     sut = new LoadEventByIdController()
   })
   it('Should be an instance of Controller', () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('Should build validators correctly', () => {
+    const validators = sut.buildValidators({
+      params: { eventId }
+    })
+
+    expect(validators).toEqual([
+      new RequiredStringValidator(eventId, 'eventId'),
+      new StringValidator(eventId, 'eventId')
+    ])
   })
 })
