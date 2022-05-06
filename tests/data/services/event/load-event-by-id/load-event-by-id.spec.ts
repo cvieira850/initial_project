f8639b1bf9ea5951212be2584d84f77c1cf45e46
Caio Vieira <contato@caiovieira.com.br>
2022-05-06 17:54:29 -0300

    test: ensure LoadEventByIdService calls LoadEventRepository with correct id
    issue: #50

---
+++ tests/data/services/event/load-event-by-id/load-event-by-id.spec.ts
@@ -0,0 +1,34 @@
+import { LoadEventByIdService } from '@/data/services'
+import { LoadEventByIdRepository } from '@/data/protocols/db'
+
+import { mock, MockProxy } from 'jest-mock-extended'
+
+describe('LoadEventByIdService', () => {
+  let sut: LoadEventByIdService
+  let loadEventByIdRepository: MockProxy<LoadEventByIdRepository>
+  let id: string
+
+  beforeAll(() => {
+    id = 'any_id'
+    loadEventByIdRepository = mock()
+    loadEventByIdRepository.loadById.mockResolvedValue({
+      id,
+      name: 'any_name',
+      description: 'any_description',
+      user_id: 'any_id',
+      created_at: new Date()
+
+    })
+  })
+
+  beforeEach(() => {
+    sut = new LoadEventByIdService(loadEventByIdRepository)
+  })
+
+  it('Should call LoadEventByIdRepository with correct Id', async () => {
+    await sut.perform({ id })
+
+    expect(loadEventByIdRepository.loadById).toHaveBeenCalledWith({ id })
+    expect(loadEventByIdRepository.loadById).toHaveBeenCalledTimes(1)
+  })
+})
