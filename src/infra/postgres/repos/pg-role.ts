a16964217d2f7dcff3c8d113d686d5523e497052
Caio Vieira <contato@caiovieira.com.br>
2022-05-07 18:58:39 -0300

    feat: ensure PgRoleRepository return a role on LoadRoleByNameRepository success
    issue: #63

--- src/infra/postgres/repos/pg-role.ts
+++ src/infra/postgres/repos/pg-role.ts
@@ -1,5 +1,19 @@
+import { LoadRoleByNameRepository } from "@/data/protocols/db";
 import { PgRepository } from "@/infra/postgres/repos/repository";
+import { Role } from "@/infra/postgres/entities";
 
-export class PgRoleRepository extends PgRepository{
+export class PgRoleRepository extends PgRepository implements LoadRoleByNameRepository{
+  async loadByName (params: LoadRoleByNameRepository.Params): Promise<LoadRoleByNameRepository.Result>{
+    const roleRepo =  this.getRepository(Role)
+    const role = await roleRepo.findOne({ name: params.name})
+    if(role) {
+      return {
+        id: role.id.toString(),
+        name: role.name,
+        weight: role.weight,
+        created_at: role.created_at
+      }
+    }
+  }
 
 }
