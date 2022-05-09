67b388730c9131a173e98aa34e07c4ce18cb9a32
Caio Vieira <contato@caiovieira.com.br>
2022-05-09 11:37:46 -0300

    test: ensure LoadRolesService calls LoadRolesRepository once
    issue: #72

--- tests/data/services/role/load-roles/load-roles.spec.ts
+++ tests/data/services/role/load-roles/load-roles.spec.ts
@@ -1,10 +1,48 @@
 import { LoadRolesService } from '@/data/services'
+import { LoadRolesRepository } from '@/data/protocols/db'
+
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('LoadRoles Service', () => {
   let sut: LoadRolesService
+  let roleRepo: MockProxy<LoadRolesRepository>
+  let id: string
+  let name: string
+  let weight: number
+  let created_at: Date
+  let other_id: string
+  let other_name: string
+  let other_weight: number
+  let other_created_at: Date
+
+  beforeAll(() => {
+    id = 'any_id'
+    name = 'any_name'
+    weight = 1
+    created_at = new Date()
+    other_id = 'other_id'
+    other_name = 'other_name'
+    other_weight = 2
+    other_created_at = new Date()
+    roleRepo = mock()
+    roleRepo.load.mockResolvedValue([
+      {
+        id,
+        name,
+        weight,
+        created_at
+      },
+      {
+        id: other_id,
+        name: other_name,
+        weight: other_weight,
+        created_at: other_created_at
+      }
+    ])
+  })
 
   beforeEach(() => {
-    sut = new LoadRolesService()
+    sut = new LoadRolesService(roleRepo)
   })
 
   it('Should be defined', () => {
@@ -25,5 +63,13 @@ describe('LoadRoles Service', () => {
 
   it('Should implement the perform method', () => {
     expect(typeof sut.perform).toBe('function')
+  })
+
+  describe('LoadRolesRepository', () => {
+    it('Should call LoadRolesRepository once', async () => {
+      await sut.perform(null)
+
+      expect(roleRepo.load).toHaveBeenCalledTimes(1)
+    })
   })
 })
