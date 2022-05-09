2c6da889d5537a5a6a93e6760e13e35a981cbe38
Caio Vieira <contato@caiovieira.com.br>
2022-05-09 13:15:01 -0300

    feat: ensure LoadRolesController calls LoadRoles usecase with correct values
    issue: #70

--- src/application/controllers/role/load-roles/load-roles.ts
+++ src/application/controllers/role/load-roles/load-roles.ts
@@ -1,8 +1,14 @@
 import { Controller } from '@/application/controllers'
 import { HttpRequest, HttpResponse } from '@/application/helpers'
+import { LoadRoles } from '@/domain/usecases'
 
 export class LoadRolesController extends Controller {
+  constructor (private readonly loadRoles: LoadRoles) {
+    super()
+  }
+
   async perform (httpRequest: HttpRequest): Promise<HttpResponse<any>> {
+    await this.loadRoles.perform(null)
     throw new Error('Method not implemented.')
   }
 }
