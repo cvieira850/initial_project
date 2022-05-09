db65932537ffa453c147999d62fdda842fe52bc5
Caio Vieira <contato@caiovieira.com.br>
2022-05-09 08:39:19 -0300

    test: ensure AddRoleController call AddRole with correct values
    issue: #60

--- tests/application/controllers/role/add-role/add-role.spec.ts
+++ tests/application/controllers/role/add-role/add-role.spec.ts
@@ -1,18 +1,33 @@
 import { AddRoleController, Controller } from '@/application/controllers'
+import { AddRole } from '@/domain/usecases'
 import { NumberValidator, RequiredValidator, StringValidator } from '@/application/validation'
+
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('AddRoleController', () => {
   let sut: AddRoleController
+  let addRole: MockProxy<AddRole>
+  let id: string
   let name: string
   let weight: number
+  let created_at: Date
 
   beforeAll(() => {
+    id = 'any_id'
     name = 'any_name'
     weight = 1
+    created_at = new Date()
+    addRole = mock()
+    addRole.perform.mockResolvedValue({
+      id,
+      name,
+      weight,
+      created_at
+    })
   })
 
   beforeEach(async () => {
-    sut = new AddRoleController()
+    sut = new AddRoleController(addRole)
   })
 
   it('Should be an instance of Controller', () => {
@@ -30,5 +45,14 @@ describe('AddRoleController', () => {
       new RequiredValidator(weight, 'weight'),
       new NumberValidator(weight, 'weight')
     ])
+  })
+
+  describe('Add Role', () => {
+    it('Should call AddRole with correct values', async () => {
+      await sut.handle({ body: { name, weight } })
+
+      expect(addRole.perform).toHaveBeenCalledWith({ name, weight })
+      expect(addRole.perform).toHaveBeenCalledTimes(1)
+    })
   })
 })
