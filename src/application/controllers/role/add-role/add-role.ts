f73a7cf2d5ed29b2e1c0532c5df941f38d46a1f5
Caio Vieira <contato@caiovieira.com.br>
2022-05-09 08:38:42 -0300

    feat: ensure AddRoleController call AddRole with correct values
    issue: #60

--- src/application/controllers/role/add-role/add-role.ts
+++ src/application/controllers/role/add-role/add-role.ts
@@ -1,9 +1,15 @@
 import { Controller } from '@/application/controllers'
 import { HttpRequest, HttpResponse } from '@/application/helpers'
 import { ValidationBuilder as Builder, Validator } from '@/application/validation'
+import { AddRole } from '@/domain/usecases'
 
 export class AddRoleController extends Controller {
+  constructor (private readonly addRole: AddRole) {
+    super()
+  }
+
   async perform (httpRequest: HttpRequest): Promise<HttpResponse<any>> {
+    await this.addRole.perform({ name: httpRequest.body.name, weight: httpRequest.body.weight })
     throw new Error('Method not implemented.')
   }
 
