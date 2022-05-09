b65cc0792a2645c527ff4eed34c40a96f8189d0c
Caio Vieira <contato@caiovieira.com.br>
2022-05-09 13:07:54 -0300

    feat: ensure LoadRolesController be an instance of Controller
    issue: #70

---
+++ src/application/controllers/role/load-roles/load-roles.ts
@@ -0,0 +1,8 @@
+import { Controller } from '@/application/controllers'
+import { HttpRequest, HttpResponse } from '@/application/helpers'
+
+export class LoadRolesController extends Controller {
+  async perform (httpRequest: HttpRequest): Promise<HttpResponse<any>> {
+    throw new Error('Method not implemented.')
+  }
+}
