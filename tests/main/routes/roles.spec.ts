f70bc7e2fff5d0a04c0b63dbc9d80cd87e7f1a16
Caio Vieira <contato@caiovieira.com.br>
2022-05-09 09:21:51 -0300

    test: ensure AddRoleController return 400 if invalid param was send
    issue: #61"

--- tests/main/routes/roles.spec.ts
+++ tests/main/routes/roles.spec.ts
@@ -1,3 +1,4 @@
+import { InvalidParamError } from '@/application/errors'
 import { Role } from '@/infra/postgres/entities'
 import { PgConnection } from '@/infra/postgres/helpers'
 import { app } from '@/main/config/app'
@@ -31,6 +32,18 @@ describe('Role routes', () => {
       expect(status).toBe(200)
       expect(body.name).toBe('user')
       expect(body.weight).toBe(1)
+    })
+
+    it('should return 400 with InvalidPararError', async () => {
+      await request(app)
+        .post('/api/roles')
+        .send({ name: 'user', weight: '1' })
+        .expect(400, { error: new InvalidParamError('weight').message })
+
+      await request(app)
+        .post('/api/roles')
+        .send({ name: 2, weight: 1 })
+        .expect(400, { error: new InvalidParamError('name').message })
     })
   })
 })
