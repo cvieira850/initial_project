a2beddd306f17476247a38acdec06726efda457d
Caio Vieira <contato@caiovieira.com.br>
2022-05-10 17:07:38 -0300

    feat: ensure LoadRoleByIdController build validators correctly
    issue: #77

--- src/application/controllers/role/load-role-by-id/load-role-by-id.ts
+++ src/application/controllers/role/load-role-by-id/load-role-by-id.ts
@@ -1,8 +1,15 @@
 import { Controller } from '@/application/controllers'
 import { HttpRequest, HttpResponse } from '@/application/helpers'
+import { ValidationBuilder as Builder, Validator } from '@/application/validation'
 
 export class LoadRoleByIdController extends Controller {
   async perform (httpRequest: HttpRequest): Promise<HttpResponse<any>> {
     throw new Error('Method not implemented.')
+  }
+
+  override buildValidators (httpRequest: HttpRequest): Validator[] {
+    return [
+      ...Builder.of({ value: httpRequest.params.roleId, fieldName: 'roleId' }).required().string().build()
+    ]
   }
 }
