291861648d524477051b7899341264b57b0c2fcb
Caio Vieira <contato@caiovieira.com.br>
2022-05-10 20:20:31 -0300

    feat: ensure Load Role By Id route return 200 with a role on success
    issue: #81 #82

--- src/main/routes/roles.ts
+++ src/main/routes/roles.ts
@@ -1,5 +1,5 @@
 import { adaptExpressRoute as adapt } from '@/main/adapters'
-import { makeAddRoleController, makeLoadRolesController } from '@/main/factories/controllers'
+import { makeAddRoleController, makeLoadRolesController, makeLoadRoleByIdController } from '@/main/factories/controllers'
 import { sysAdminAuth } from '@/main/middlewares'
 
 import { Router } from 'express'
@@ -7,12 +7,15 @@ import { Router } from 'express'
 export default (router: Router): void => {
   const controller = makeAddRoleController()
   const loadRolesController = makeLoadRolesController()
+  const loadRoleByIdController = makeLoadRoleByIdController()
   const isProd = process.env.NODE_ENV === 'production'
   if (isProd) {
     router.post('/roles', sysAdminAuth, adapt(controller))
     router.get('/roles', sysAdminAuth, adapt(loadRolesController))
+    router.get('/roles/:roleId', sysAdminAuth, adapt(loadRoleByIdController))
   } else {
     router.post('/roles', adapt(controller))
     router.get('/roles', adapt(loadRolesController))
+    router.get('/roles/:roleId', adapt(loadRoleByIdController))
   }
 }
