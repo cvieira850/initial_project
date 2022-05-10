719306625975d222459436dc16e6dd32f485554d
Caio Vieira <contato@caiovieira.com.br>
2022-05-10 17:08:14 -0300

    test: ensure LoadRoleByIdController build validators correctly
    issue: #77

--- tests/application/controllers/role/load-role-by-id/load-role-by-id.spec.ts
+++ tests/application/controllers/role/load-role-by-id/load-role-by-id.spec.ts
@@ -1,13 +1,27 @@
 import { Controller, LoadRoleByIdController } from '@/application/controllers'
+import { RequiredValidator, StringValidator } from '@/application/validation'
 
 describe('LoadRoleByIdController', () => {
   let sut: LoadRoleByIdController
+  let roleId: string
 
+  beforeAll(() => {
+    roleId = 'any_id'
+  })
   beforeEach(() => {
     sut = new LoadRoleByIdController()
   })
 
   it('Should be an instance of Controller', () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('Should build Validators correctly', async () => {
+    const validators = sut.buildValidators({ params: { roleId } })
+
+    expect(validators).toEqual([
+      new RequiredValidator(roleId, 'roleId'),
+      new StringValidator(roleId, 'roleId')
+    ])
   })
 })
