00f23751c703d38397734b876234f3ce8284a0f4
Caio Vieira <contato@caiovieira.com.br>
2022-05-10 16:32:51 -0300

    feat: ensure LoadRoleByIdService call LoadRoleByIdRepository with correct id
    issue: #79

--- src/data/services/role/load-role-by-id/load-role-by-id.ts
+++ src/data/services/role/load-role-by-id/load-role-by-id.ts
@@ -1,7 +1,9 @@
-import { LoadRoleById } from './load-role-by-id-protocols'
+import { LoadRoleById, LoadRoleByIdRepository } from './load-role-by-id-protocols'
 
 export class LoadRoleByIdService implements LoadRoleById {
+  constructor (private readonly loadRoleByIdRepository: LoadRoleByIdRepository) {}
   async perform (params: LoadRoleById.Params): Promise<LoadRoleById.Result> {
+    await this.loadRoleByIdRepository.loadById({ id: params.id })
     return undefined
   }
 }
