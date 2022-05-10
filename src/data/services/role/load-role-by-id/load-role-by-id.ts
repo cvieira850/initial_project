4e5b6efed51c9a6c85046f3ae2e927f7751a003f
Caio Vieira <contato@caiovieira.com.br>
2022-05-10 16:47:09 -0300

    feat: ensure LoadRoleByIdService return a role on LoadRoleByIdRepository success
    issue: #79

--- src/data/services/role/load-role-by-id/load-role-by-id.ts
+++ src/data/services/role/load-role-by-id/load-role-by-id.ts
@@ -3,7 +3,6 @@ import { LoadRoleById, LoadRoleByIdRepository } from './load-role-by-id-protocol
 export class LoadRoleByIdService implements LoadRoleById {
   constructor (private readonly loadRoleByIdRepository: LoadRoleByIdRepository) {}
   async perform (params: LoadRoleById.Params): Promise<LoadRoleById.Result> {
-    await this.loadRoleByIdRepository.loadById({ id: params.id })
-    return undefined
+    return await this.loadRoleByIdRepository.loadById({ id: params.id })
   }
 }
