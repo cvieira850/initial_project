ca5eea4a332010815959a774b438a5c8e479d304
Caio Vieira <contato@caiovieira.com.br>
2022-05-11 13:41:08 -0300

    feat: ensure UpdateRoleService calls UpdateRoleRepository with correct values
    issue: #90

--- src/data/services/role/update-role/update-role.ts
+++ src/data/services/role/update-role/update-role.ts
@@ -1,13 +1,17 @@
 import { InvalidRequestError } from '@/data/errors'
-import { UpdateRole, LoadRoleByIdRepository } from './update-role-protocols'
+import { UpdateRole, LoadRoleByIdRepository, UpdateRoleRepository } from './update-role-protocols'
 
 export class UpdateRoleService implements UpdateRole {
-  constructor (private readonly roleRepo: LoadRoleByIdRepository) {}
+  constructor (
+    private readonly roleRepo: LoadRoleByIdRepository & UpdateRoleRepository
+  ) {}
+
   async perform (params: UpdateRole.Params): Promise<UpdateRole.Result> {
     const role = await this.roleRepo.loadById({ id: params.id })
     if (!role) {
       throw new InvalidRequestError('Role not found')
     }
+    await this.roleRepo.update({ id: params.id, name: params.name, weight: params.weight })
     return undefined
   }
 }
