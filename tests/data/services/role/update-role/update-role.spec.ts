e0fe3ea0571738ef39bdc4c6284314ec9856e1d9
Caio Vieira <contato@caiovieira.com.br>
2022-05-11 13:41:51 -0300

    test: ensure UpdateRoleService calls UpdateRoleRepository with correct values
    issue: #90

--- tests/data/services/role/update-role/update-role.spec.ts
+++ tests/data/services/role/update-role/update-role.spec.ts
@@ -1,27 +1,37 @@
 import { UpdateRoleService } from '@/data/services'
-import { LoadRoleByIdRepository } from '@/data/protocols/db'
+import { LoadRoleByIdRepository, UpdateRoleRepository } from '@/data/protocols/db'
 
 import { mock, MockProxy } from 'jest-mock-extended'
 import { InvalidRequestError } from '@/data/errors'
 
 describe('UpdateRoleService', () => {
   let sut: UpdateRoleService
-  let roleRepo: MockProxy<LoadRoleByIdRepository>
+  let roleRepo: MockProxy<LoadRoleByIdRepository & UpdateRoleRepository>
   let id: string
   let name: string
   let weight: number
   let created_at: Date
+  let new_name: string
+  let new_weight: number
 
   beforeAll(() => {
     id = 'any_id'
     name = 'any_name'
     weight = 1
     created_at = new Date()
+    new_name = 'new_name'
+    new_weight = 2
     roleRepo = mock()
     roleRepo.loadById.mockResolvedValue({
       id,
       name,
       weight,
+      created_at
+    })
+    roleRepo.update.mockResolvedValue({
+      id,
+      name: new_name,
+      weight: new_weight,
       created_at
     })
   })
@@ -52,6 +62,15 @@ describe('UpdateRoleService', () => {
       const promise = sut.perform({ id })
 
       await expect(promise).rejects.toThrow(new InvalidRequestError('Role not found'))
+    })
+  })
+
+  describe('UpdateRoleRepository', () => {
+    it('Should call UpdateRoleRepository with correct values', async () => {
+      await sut.perform({ id, name, weight })
+
+      expect(roleRepo.update).toHaveBeenCalledWith({ id, name, weight })
+      expect(roleRepo.update).toHaveBeenCalledTimes(1)
     })
   })
 })
