8a3ca38036fb80c9d3a2b3d8d128fa99342eb8d1
Caio Vieira <contato@caiovieira.com.br>
2022-05-11 17:18:57 -0300

    test: ensure UpdateRoleController build validators correctly
    issue: #88

--- tests/application/controllers/role/update-role/update-role.spec.ts
+++ tests/application/controllers/role/update-role/update-role.spec.ts
@@ -1,7 +1,17 @@
 import { Controller, UpdateRoleController } from '@/application/controllers'
+import { NumberValidator, RequiredValidator, StringValidator } from '@/application/validation'
 
 describe('UpdateRole Repository', () => {
   let sut: UpdateRoleController
+  let roleId: string
+  let name: string
+  let weight: number
+
+  beforeAll(() => {
+    roleId = 'any_id'
+    name = 'any_name'
+    weight = 1
+  })
 
   beforeEach(() => {
     sut = new UpdateRoleController()
@@ -9,5 +19,21 @@ describe('UpdateRole Repository', () => {
 
   it('Should be an instance of Controller', () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('Should build Validators correctly', async () => {
+    const validators = sut.buildValidators({
+      params: { roleId },
+      body: { name, weight }
+    })
+
+    expect(validators).toEqual([
+      new RequiredValidator(roleId, 'roleId'),
+      new StringValidator(roleId, 'roleId'),
+      new RequiredValidator(name, 'name'),
+      new StringValidator(name, 'name'),
+      new RequiredValidator(weight, 'weight'),
+      new NumberValidator(weight, 'weight')
+    ])
   })
 })
