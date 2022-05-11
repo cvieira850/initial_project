2e962e5396624e1bff43fb0133c04a2228aae44c
Caio Vieira <contato@caiovieira.com.br>
2022-05-11 17:11:26 -0300

    feat: ensure UpdateRoleController build validators correctly
    issue: #88

--- src/application/controllers/role/update-role/update-role.ts
+++ src/application/controllers/role/update-role/update-role.ts
@@ -1,8 +1,17 @@
 import { HttpRequest, HttpResponse } from '@/application/helpers'
+import { ValidationBuilder as Builder, Validator } from '@/application/validation'
 import { Controller } from '@/application/controllers'
 
 export class UpdateRoleController extends Controller {
   async perform (httpRequest: HttpRequest): Promise<HttpResponse<any>> {
     throw new Error('Method not implemented.')
+  }
+
+  override buildValidators (httpRequest: HttpRequest): Validator[] {
+    return [
+      ...Builder.of({ value: httpRequest.params.roleId, fieldName: 'roleId' }).required().string().build(),
+      ...Builder.of({ value: httpRequest.body.name, fieldName: 'name' }).required().string().build(),
+      ...Builder.of({ value: httpRequest.body.weight, fieldName: 'weight' }).required().number().build()
+    ]
   }
 }
