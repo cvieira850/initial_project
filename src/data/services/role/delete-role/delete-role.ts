9134d0253a7ab61c1808869d648c2cb61b6d9519
Caio Vieira <contato@caiovieira.com.br>
2022-05-12 18:58:57 -0300

    feat: ensure DeleteRoleService return an error if LoadRoleByIdRepository returns undefined
    issue: #101

--- src/data/services/role/delete-role/delete-role.ts
+++ src/data/services/role/delete-role/delete-role.ts
@@ -1,9 +1,13 @@
 import { DeleteRole, LoadRoleByIdRepository } from './delete-role-protocols'
+import { InvalidRequestError } from '@/data/errors'
 
 export class DeleteRoleService implements DeleteRole {
   constructor (private readonly roleRepo: LoadRoleByIdRepository) {}
   async perform (params: DeleteRole.Params): Promise<DeleteRole.Result> {
-    await this.roleRepo.loadById({ id: params.id })
+    const role = await this.roleRepo.loadById({ id: params.id })
+    if (!role) {
+      throw new InvalidRequestError('Role not found')
+    }
     return undefined
   }
 }
