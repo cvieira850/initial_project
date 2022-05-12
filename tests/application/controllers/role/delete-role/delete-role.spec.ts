220ca4884119001e54678944fad9ea6a019dcd84
Caio Vieira <contato@caiovieira.com.br>
2022-05-12 19:29:25 -0300

    test: ensure DeleteRoleController build Validators correctly
    issue: #99

--- tests/application/controllers/role/delete-role/delete-role.spec.ts
+++ tests/application/controllers/role/delete-role/delete-role.spec.ts
@@ -1,7 +1,13 @@
 import { Controller, DeleteRoleController } from '@/application/controllers'
+import { RequiredValidator, StringValidator } from '@/application/validation'
 
 describe('DeleteRole Controller', () => {
   let sut: DeleteRoleController
+  let roleId: string
+
+  beforeAll(() => {
+    roleId = 'any_id'
+  })
 
   beforeEach(() => {
     sut = new DeleteRoleController()
@@ -9,5 +15,16 @@ describe('DeleteRole Controller', () => {
 
   it('Should be an instance of Controller', () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('Should build Validators correctly', async () => {
+    const validators = sut.buildValidators({
+      params: { roleId }
+    })
+
+    expect(validators).toEqual([
+      new RequiredValidator(roleId, 'roleId'),
+      new StringValidator(roleId, 'roleId')
+    ])
   })
 })
