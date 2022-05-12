a2eee8e32c2e7c1a316c06c66105b62ae361d6ae
Caio Vieira <contato@caiovieira.com.br>
2022-05-12 19:47:14 -0300

    feat: ensure DeleteRoleController return 401 if DeleteRole throw invalidRequestError
    issue: #99

--- src/application/controllers/role/delete-role/delete-role.ts
+++ src/application/controllers/role/delete-role/delete-role.ts
@@ -1,7 +1,8 @@
-import { HttpRequest, HttpResponse } from '@/application/helpers'
+import { HttpRequest, HttpResponse, unauthorized } from '@/application/helpers'
 import { Controller } from '@/application/controllers'
 import { ValidationBuilder as Builder, Validator } from '@/application/validation'
 import { DeleteRole } from '@/domain/usecases'
+import { InvalidRequestError } from '@/data/errors'
 
 export class DeleteRoleController extends Controller {
   constructor (private readonly usecase: DeleteRole) {
@@ -9,8 +10,15 @@ export class DeleteRoleController extends Controller {
   }
 
   async perform (httpRequest: HttpRequest): Promise<HttpResponse<any>> {
-    await this.usecase.perform({ id: httpRequest.params.roleId })
-    throw new Error('Method not implemented.')
+    try {
+      await this.usecase.perform({ id: httpRequest.params.roleId })
+      throw new Error('Method not implemented.')
+    } catch (error) {
+      if (error instanceof InvalidRequestError) {
+        return unauthorized()
+      }
+      throw error
+    }
   }
 
   override buildValidators (httpRequest: HttpRequest): Validator[] {
