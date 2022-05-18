6d41c6a96b90a2df1523800654dddd7215ea34fb
Caio Vieira <contato@caiovieira.com.br>
2022-05-18 20:11:08 -0300

    test: ensure UpdateAccountUserController build Validators correctly
    issue: #108

--- tests/application/controllers/account/update-account-user/update-account-user.spec.ts
+++ tests/application/controllers/account/update-account-user/update-account-user.spec.ts
@@ -1,8 +1,15 @@
-
 import { Controller, UpdateAccountUserController } from '@/application/controllers'
+import { RequiredValidator, StringValidator } from '@/application/validation'
 
 describe('UpdateAccountUserController', () => {
   let sut: UpdateAccountUserController
+  let userId: string
+  let roleId: string
+
+  beforeAll(() => {
+    userId = 'any_id'
+    roleId = 'any_role_id'
+  })
 
   beforeEach(() => {
     sut = new UpdateAccountUserController()
@@ -10,5 +17,16 @@ describe('UpdateAccountUserController', () => {
 
   it('Should be an instance of Controller', () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('Should build Validators correctly', async () => {
+    const validators = sut.buildValidators({ params: { userId }, body: { roleId } })
+
+    expect(validators).toEqual([
+      new RequiredValidator(userId, 'userId'),
+      new StringValidator(userId, 'userId'),
+      new RequiredValidator(roleId, 'roleId'),
+      new StringValidator(roleId, 'roleId')
+    ])
   })
 })
