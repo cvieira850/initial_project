61729e0b99f916d2bbb4720102e59775d4b6a88f
Caio Vieira <contato@caiovieira.com.br>
2022-05-18 20:10:29 -0300

    feat: ensure UpdateAccountUserController build Validators correctly
    issue: #108

--- src/application/controllers/account/update-account-user/update-account-user.ts
+++ src/application/controllers/account/update-account-user/update-account-user.ts
@@ -1,8 +1,16 @@
 import { HttpRequest, HttpResponse } from '@/application/helpers'
+import { ValidationBuilder as Builder, Validator } from '@/application/validation'
 import { Controller } from '@/application/controllers'
 
 export class UpdateAccountUserController extends Controller {
   async perform (httpRequest: HttpRequest): Promise<HttpResponse<any>> {
     throw new Error('Method not implemented.')
+  }
+
+  override buildValidators (httpRequest: HttpRequest): Validator[] {
+    return [
+      ...Builder.of({ value: httpRequest.params.userId, fieldName: 'userId' }).required().string().build(),
+      ...Builder.of({ value: httpRequest.body.roleId, fieldName: 'roleId' }).required().string().build()
+    ]
   }
 }
