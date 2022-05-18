40421d85d2cac5bcffed3d8b4cebc8798f3dffff
Caio Vieira <contato@caiovieira.com.br>
2022-05-18 19:54:44 -0300

    test: ensure PgUserRepository return undefined dont return a role
    issue: #111

--- tests/infra/postgres/repos/account.spec.ts
+++ tests/infra/postgres/repos/account.spec.ts
@@ -159,10 +159,19 @@ describe('AccountPgRepository', () => {
       expect(user).toEqual({ id: '1', name: 'any_name', role: 'admin', email: 'any_email' })
     })
 
-    it('Should return undefined if updateAccountRole dont return a user', async () => {
+    it('Should return undefined if updateAccountRole dont return an user', async () => {
       const user = await sut.updateAccountRole({ id: '1', roleId: '2' })
 
       expect(user).toBeUndefined()
     })
-  });
+
+    it('Should return undefined if updateAccountRole dont return a role', async () => {
+      await pgRoleRepo.save({name: 'user', weight: 1})
+      await pgUserRepo.save({ email: 'any_email', name: 'any_name', password: '1234', role_id: '1' })
+
+      const user = await sut.updateAccountRole({ id: '1', roleId: '2' })
+
+      expect(user).toBeUndefined()
+    })
+  })
 })
