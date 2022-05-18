48a179e4057bf26c450471882e49d2fe24d6c2d1
Caio Vieira <contato@caiovieira.com.br>
2022-05-18 17:06:01 -0300

    feat: ensure UpdateAccountRole Service calls UpdateAccountRoleRepository with correct params
    issue: #110

--- src/data/services/account/update-account-role/update-account-role.ts
+++ src/data/services/account/update-account-role/update-account-role.ts
@@ -1,15 +1,18 @@
-import { UpdateAccountRole, LoadRoleByIdRepository, LoadAccountByIdRepository } from './update-account-role-protocols'
+import { UpdateAccountRole, LoadRoleByIdRepository, LoadAccountByIdRepository, UpdateAccountRoleRepository } from './update-account-role-protocols'
 
 export class UpdateAccountRoleService implements UpdateAccountRole {
   constructor (
     private readonly roleRepo: LoadRoleByIdRepository,
-    private readonly accountRepo: LoadAccountByIdRepository
+    private readonly accountRepo: LoadAccountByIdRepository & UpdateAccountRoleRepository
   ) {}
 
   async perform (params: UpdateAccountRole.Params): Promise<UpdateAccountRole.Result> {
     const role = await this.roleRepo.loadById({ id: params.roleId })
     if (role) {
-      await this.accountRepo.loadById({ id: params.id })
+      const user = await this.accountRepo.loadById({ id: params.id })
+      if (user) {
+        await this.accountRepo.updateAccountRole({ id: params.id, roleId: params.roleId })
+      }
     }
     return undefined
   }
