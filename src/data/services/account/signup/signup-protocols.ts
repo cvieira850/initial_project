561ee74a259d109b06e9baa6e9be01542739e14f
Caio Vieira <contato@caiovieira.com.br>
2022-05-19 18:34:20 -0300

    feat: add send email on create user and new logger

--- src/data/services/account/signup/signup-protocols.ts
+++ src/data/services/account/signup/signup-protocols.ts
@@ -1,3 +1,4 @@
 export * from '@/data/protocols/db'
 export * from '@/domain/usecases'
 export * from '@/data/protocols/cryptography'
+export * from '@/data/protocols/email'
--- src/data/services/account/signup/signup.ts
+++ src/data/services/account/signup/signup.ts
@@ -1,10 +1,11 @@
-import { LoadAccountByEmailRepository, Signup, Hash, AddAccountRepository, Encrypt, UpdateAccessTokenRepository } from './signup-protocols'
+import { SendEmail, LoadAccountByEmailRepository, Signup, Hash, AddAccountRepository, Encrypt, UpdateAccessTokenRepository } from './signup-protocols'
 
 export class SignupService implements Signup {
   constructor (
     private readonly accountRepo: LoadAccountByEmailRepository & AddAccountRepository & UpdateAccessTokenRepository,
     private readonly hash: Hash,
-    private readonly encrypt: Encrypt
+    private readonly encrypt: Encrypt,
+    private readonly sendEmail: SendEmail
   ) {}
 
   public perform = async ({ email, name, password }: Signup.Params): Promise<Signup.Result> => {
@@ -17,8 +18,30 @@ export class SignupService implements Signup {
           const accessToken = await this.encrypt.encrypt({ plaintext: accountLoaded.id })
           const updatedAccount = await this.accountRepo.updateAccessToken({ id: accountLoaded.id, accessToken })
           if (updatedAccount) {
-            return {
-              accessToken
+            const result = await this.sendEmail.send({
+              to: updatedAccount.email,
+              html: `
+                <!DOCTYPE html>
+                <html lang="pt-br">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Bem vindo ${updatedAccount.name}!</title>
+                </head>
+                <body>
+                    <div style="padding:32px; max-width: 735px;">
+                        <h1>Seja bem vindo ${updatedAccount.name}</h1>
+                    </div>
+                </body>
+                </html>
+              `,
+              subject: 'Bem vindo!'
+            })
+            if (result) {
+              return {
+                accessToken
+              }
             }
           }
         }
