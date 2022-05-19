864367a57ff5a923a153c49388c4cc6aa93548c0
Caio Vieira <contato@caiovieira.com.br>
2022-05-19 20:12:33 -0300

    feat: add forgot password usecase on domain layer
    issue: #16

---
+++ src/domain/usecases/account/forgot-password.ts
@@ -0,0 +1,13 @@
+import { Account } from '@/domain/models'
+
+export interface ForgotPassword {
+  perform: (params: ForgotPassword.Params) => Promise<ForgotPassword.Result>
+}
+
+export namespace ForgotPassword {
+  export type Params = {
+    mail: string
+  }
+
+  export type Result = Account | undefined
+}
