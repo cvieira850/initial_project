750a7e75fc3e0e88e6521e3e6933025813a58dbc
Caio Vieira <contato@caiovieira.com.br>
2022-05-19 20:54:12 -0300

    feat: ensure ForgotPasswordService call SendMail once
    issue: #17

--- src/data/services/account/forgot-password/forgot-password.ts
+++ src/data/services/account/forgot-password/forgot-password.ts
@@ -1,8 +1,40 @@
-import { ForgotPassword, LoadAccountByEmailRepository } from './forgot-password-protocols'
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+import { SendEmail, ForgotPassword, LoadAccountByEmailRepository } from './forgot-password-protocols'
 
 export class ForgotPasswordService implements ForgotPassword {
-  constructor (private readonly accountRepo: LoadAccountByEmailRepository) {}
+  constructor (
+    private readonly accountRepo: LoadAccountByEmailRepository,
+    private readonly sendEmail: SendEmail
+  ) {}
+
   async perform (params: ForgotPassword.Params): Promise<ForgotPassword.Result> {
-    return await this.accountRepo.loadByEmail({ email: params.email })
+    const account = await this.accountRepo.loadByEmail({ email: params.email })
+    if (account) {
+      const result = await this.sendEmail.send({
+        to: account.email,
+        html: `
+        <!DOCTYPE html>
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Recuperação de senha</title>
+        </head>
+        <body>
+            <div style="padding:32px; max-width: 735px;">
+                <h1>Olá ${account.name}</h1>
+                <p>Você solicitou uma recuperação de senha, clique no link abaixo para criar uma nova senha.</p>
+                <a href="http://localhost:3000/reset-password?token=${account.reset_password_token}">Recuperar senha</a>
+            </div>
+        </body>
+        </html>
+      `,
+        subject: 'Recuperação de senha'
+      })
+      if (result) {
+        return account
+      }
+    }
   }
 }
