db13e0c89d6cb2b780fadbcfc133aacb61900b50
Caio Vieira <contato@caiovieira.com.br>
2022-05-20 11:09:16 -0300

    test: ensure ForgotPasswordController build Validators correctly
    issue: #14

--- tests/application/controllers/account/forgot-password/forgot-password.spec.ts
+++ tests/application/controllers/account/forgot-password/forgot-password.spec.ts
@@ -1,13 +1,26 @@
 import { Controller, ForgotPasswordController } from '@/application/controllers'
+import { EmailValidation, EmailValidatorAdapter, RequiredValidator, StringValidator } from '@/application/validation'
 
 describe('ForgotPassword Controller', () => {
   let sut: ForgotPasswordController
+  let email: string
 
   beforeEach(() => {
+    email = 'any_email'
     sut = new ForgotPasswordController()
   })
 
   it('Should be an instance of Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('Should build Validators correctly', async () => {
+    const validators = sut.buildValidators({ body: { email } })
+
+    expect(validators).toEqual([
+      new RequiredValidator(email, 'email'),
+      new EmailValidation(email, 'email', new EmailValidatorAdapter()),
+      new StringValidator(email, 'email')
+    ])
   })
 })
