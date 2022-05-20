eb1ee8a405f68067e7d65b84640287592cb593e8
Caio Vieira <contato@caiovieira.com.br>
2022-05-20 11:08:28 -0300

    feat: ensure ForgotPasswordController build Validators correctly
    issue: #14

--- src/application/controllers/account/forgot-password/forgot-password.ts
+++ src/application/controllers/account/forgot-password/forgot-password.ts
@@ -1,8 +1,15 @@
 import { HttpRequest, HttpResponse } from '@/application/helpers'
 import { Controller } from '@/application/controllers'
+import { ValidationBuilder as Builder, Validator } from '@/application/validation'
 
 export class ForgotPasswordController extends Controller {
   async perform (httpRequest: HttpRequest): Promise<HttpResponse<any>> {
     throw new Error('Method not implemented.')
+  }
+
+  override buildValidators (httpRequest: HttpRequest): Validator[] {
+    return [
+      ...Builder.of({ value: httpRequest.body.email, fieldName: 'email' }).required().validateEmail().string().build()
+    ]
   }
 }
