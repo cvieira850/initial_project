7a698753b5da2d741575ef5d25a8c2ec563a0a9b
Caio Vieira <contato@caiovieira.com.br>
2022-05-20 11:23:55 -0300

    feat: ensure ForgotPasswordController call ForgotPassword with correct values
    issue: #14

--- src/application/controllers/account/forgot-password/forgot-password.ts
+++ src/application/controllers/account/forgot-password/forgot-password.ts
@@ -1,10 +1,17 @@
-import { HttpRequest, HttpResponse } from '@/application/helpers'
+import { HttpRequest, HttpResponse, noContent } from '@/application/helpers'
 import { Controller } from '@/application/controllers'
 import { ValidationBuilder as Builder, Validator } from '@/application/validation'
+import { ForgotPassword } from '@/domain/usecases'
 
 export class ForgotPasswordController extends Controller {
+  constructor (private readonly forgotPassword: ForgotPassword) {
+    super()
+  }
+
   async perform (httpRequest: HttpRequest): Promise<HttpResponse<any>> {
-    throw new Error('Method not implemented.')
+    await this.forgotPassword.perform({ email: httpRequest.body.email })
+
+    return noContent()
   }
 
   override buildValidators (httpRequest: HttpRequest): Validator[] {
