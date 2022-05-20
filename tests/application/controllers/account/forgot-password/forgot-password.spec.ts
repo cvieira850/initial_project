93341792f17ee422d3cf525a5b77461241e63393
Caio Vieira <contato@caiovieira.com.br>
2022-05-20 11:24:40 -0300

    test: ensure ForgotPasswordController call ForgotPassword with correct values
    issue: #14

--- tests/application/controllers/account/forgot-password/forgot-password.spec.ts
+++ tests/application/controllers/account/forgot-password/forgot-password.spec.ts
@@ -1,13 +1,39 @@
 import { Controller, ForgotPasswordController } from '@/application/controllers'
 import { EmailValidation, EmailValidatorAdapter, RequiredValidator, StringValidator } from '@/application/validation'
+import { ForgotPassword } from '@/domain/usecases'
+
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('ForgotPassword Controller', () => {
   let sut: ForgotPasswordController
+  let forgotPassword: MockProxy<ForgotPassword>
+  let id: string
   let email: string
+  let password: string
+  let name: string
+  let role: string
+  let reset_password_token: string
+
+  beforeAll(() => {
+    id = 'any_id'
+    email = 'any_email'
+    password = 'any_password'
+    name = 'any_name'
+    role = 'any_role'
+    reset_password_token = 'any_reset_password_token'
+    forgotPassword = mock()
+    forgotPassword.perform.mockResolvedValue({
+      id,
+      email,
+      password,
+      name,
+      role,
+      reset_password_token
+    })
+  })
 
   beforeEach(() => {
-    email = 'any_email'
-    sut = new ForgotPasswordController()
+    sut = new ForgotPasswordController(forgotPassword)
   })
 
   it('Should be an instance of Controller', async () => {
@@ -22,5 +48,14 @@ describe('ForgotPassword Controller', () => {
       new EmailValidation(email, 'email', new EmailValidatorAdapter()),
       new StringValidator(email, 'email')
     ])
+  })
+
+  describe('ForgotPassword', () => {
+    it('Should call forgotPassword with correct values', async () => {
+      await sut.perform({ body: { email } })
+
+      expect(forgotPassword.perform).toHaveBeenCalledWith({ email })
+      expect(forgotPassword.perform).toHaveBeenCalledTimes(1)
+    })
   })
 })
