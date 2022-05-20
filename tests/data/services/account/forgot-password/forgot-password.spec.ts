952aa2d5f26a8edb51690dd74a4a8cd936edd78f
Caio Vieira <contato@caiovieira.com.br>
2022-05-20 16:25:59 -0300

    test: change to update reset password token

--- tests/data/services/account/forgot-password/forgot-password.spec.ts
+++ tests/data/services/account/forgot-password/forgot-password.spec.ts
@@ -1,13 +1,15 @@
 import { ForgotPasswordService } from '@/data/services'
-import { LoadAccountByEmailRepository } from '@/data/protocols/db'
+import { LoadAccountByEmailRepository, UpdateResetPasswordTokenRepository } from '@/data/protocols/db'
 import { SendEmailNodeMailer } from '@/infra/email'
+import { Encrypt } from '@/data/protocols/cryptography'
 
 import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('ForgotPassword Service', () => {
   let sut: ForgotPasswordService
-  let accountRepo: MockProxy<LoadAccountByEmailRepository>
+  let accountRepo: MockProxy<LoadAccountByEmailRepository & UpdateResetPasswordTokenRepository>
   let sendEmail: MockProxy<SendEmailNodeMailer>
+  let encrypt: MockProxy<Encrypt>
   let id: string
   let name: string
   let email: string
@@ -28,12 +30,21 @@ describe('ForgotPassword Service', () => {
       password,
       reset_password_token
     })
+    accountRepo.updateResetPasswordToken.mockResolvedValue({
+      id,
+      name,
+      email,
+      password,
+      reset_password_token
+    })
     sendEmail = mock()
     sendEmail.send.mockResolvedValue(true)
+    encrypt = mock()
+    encrypt.encrypt.mockResolvedValue(reset_password_token)
   })
 
   beforeEach(() => {
-    sut = new ForgotPasswordService(accountRepo, sendEmail)
+    sut = new ForgotPasswordService(accountRepo, sendEmail, encrypt)
   })
 
   describe('LoadAccountByEmail Repository', () => {
@@ -73,6 +84,42 @@ describe('ForgotPassword Service', () => {
     })
   })
 
+  describe('Encrypt', () => {
+    it('Should call Encrypt with valid id once', async () => {
+      await sut.perform({ email })
+
+      expect(encrypt.encrypt).toHaveBeenCalledWith({ plaintext: id + name })
+      expect(encrypt.encrypt).toHaveBeenCalledTimes(1)
+    })
+
+    it('Should rethrow if Encrypt throws', async () => {
+      encrypt.encrypt.mockRejectedValueOnce(new Error())
+
+      const promise = sut.perform({ email })
+
+      await expect(promise).rejects.toThrow(new Error())
+    })
+  })
+
+  describe('UpdateResetPasswordToken Repository', () => {
+    it('Should call UpdateResetPasswordTokenRepository with correct values', async () => {
+      await sut.perform({ email })
+
+      expect(accountRepo.updateResetPasswordToken).toHaveBeenCalledWith({
+        id,
+        token: reset_password_token
+      })
+      expect(accountRepo.updateResetPasswordToken).toHaveBeenCalledTimes(1)
+    })
+
+    it('Should rethrow if UpdateResetPasswordTokenRepository throws', async () => {
+      accountRepo.updateResetPasswordToken.mockRejectedValueOnce(new Error())
+
+      const promise = sut.perform({ email })
+
+      await expect(promise).rejects.toThrow()
+    })
+  })
   describe('Send Email', () => {
     it('Should call sendEmail with valid id once', async () => {
       await sut.perform({ email })
