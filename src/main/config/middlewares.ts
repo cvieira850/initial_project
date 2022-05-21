a9b335a5c5855e7d833484b58e73ca690e5cb99a
Caio Vieira <contato@caiovieira.com.br>
2022-05-20 21:56:29 -0300

    refactor: remove momently two middlewares

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,16 +1,16 @@
 import { Express, json } from 'express'
 import cors from 'cors'
 import pinoHttp from 'pino-http'
-import helmet from 'helmet'
-import compression from 'compression'
+// import helmet from 'helmet'
+// import compression from 'compression'
 // import statusMonitor from 'express-status-monitor'
 import { logger } from '@/main/config/logger'
 
 export const setupMiddlewares = (app: Express): void => {
-  app.use(helmet())
+  // app.use(helmet())
   app.use(pinoHttp({ logger: logger }))
   // app.use(statusMonitor({ path: '/monitor' }))
-  app.use(compression())
+  // app.use(compression())
   app.use(cors())
   app.use(json())
   app.use((req, res, next) => {
