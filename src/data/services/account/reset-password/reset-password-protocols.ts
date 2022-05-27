67d9c7cebe063c002a74be118f3359be53dfdbc5
Caio Vieira <contato@caiovieira.com.br>
2022-05-27 19:25:00 -0300

    feat: ensure ResetPasswordService call SendEmail once
    issue: #119

--- src/data/services/account/reset-password/reset-password-protocols.ts
+++ src/data/services/account/reset-password/reset-password-protocols.ts
@@ -1,3 +1,4 @@
 export * from '@/domain/usecases'
 export * from '@/data/protocols/db'
 export * from '@/data/protocols/cryptography'
+export * from '@/data/protocols/email'
--- src/data/services/account/reset-password/reset-password.ts
+++ src/data/services/account/reset-password/reset-password.ts
@@ -1,10 +1,11 @@
-import { ResetPassword, LoadAccountByResetTokenRepository, Hash, UpdatePasswordRepository } from './reset-password-protocols'
+import { ResetPassword, LoadAccountByResetTokenRepository, Hash, UpdatePasswordRepository, SendEmail } from './reset-password-protocols'
 import { InvalidRequestError } from '@/data/errors'
 
 export class ResetPasswordService implements ResetPassword {
   constructor (
     private readonly accountRepo: LoadAccountByResetTokenRepository & UpdatePasswordRepository,
-    private readonly hash: Hash
+    private readonly hash: Hash,
+    private readonly sendEmail: SendEmail
   ) {}
 
   async perform (params: ResetPassword.Params): Promise<ResetPassword.Result> {
@@ -14,7 +15,30 @@ export class ResetPasswordService implements ResetPassword {
     }
     const hashedPassword = await this.hash.hash({ plaintext: params.password })
     if (hashedPassword) {
-      await this.accountRepo.updatePassword({ id: account.id, password: hashedPassword })
+      const updatedAccount = await this.accountRepo.updatePassword({ id: account.id, password: hashedPassword })
+      if (updatedAccount) {
+        await this.sendEmail.send({
+          to: account.email,
+          html: `
+          <!DOCTYPE html>
+          <html lang="pt-br">
+          <head>
+              <meta charset="UTF-8">
+              <meta http-equiv="X-UA-Compatible" content="IE=edge">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <title>Reset de senha</title>
+          </head>
+          <body>
+              <div style="padding:32px; max-width: 735px;">
+                  <h1>Olá ${account.name}</h1>
+                  <p>Sua senha foi alterada com sucesso.</p>
+              </div>
+          </body>
+          </html>
+        `,
+          subject: 'Reset de senha'
+        })
+      }
     }
     return undefined
   }
