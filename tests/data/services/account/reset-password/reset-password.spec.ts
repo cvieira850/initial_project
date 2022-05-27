dcbeb191d45f49b3eb324d4e7c98c61edda11fc2
Caio Vieira <contato@caiovieira.com.br>
2022-05-27 18:33:38 -0300

    test: ensure ResetPasswordService call LoadAccountByResetTokenRepository with correct values
    issue: #119

---
+++ tests/data/services/account/reset-password/reset-password.spec.ts
@@ -0,0 +1,41 @@
+import { ResetPasswordService } from '@/data/services'
+import { LoadAccountByResetTokenRepository } from '@/data/protocols/db'
+
+import { mock, MockProxy } from 'jest-mock-extended'
+
+describe('ResetPassword Service', () => {
+  let sut: ResetPasswordService
+  let accountRepo: MockProxy<LoadAccountByResetTokenRepository>
+  let id: string
+  let token: string
+  let password: string
+  let name: string
+  let email: string
+
+  beforeAll(() => {
+    id = 'any_id'
+    token = 'any_token'
+    password = 'any_password'
+    name = 'any_name'
+    email = 'any_email'
+    accountRepo = mock()
+    accountRepo.loadByResetToken.mockResolvedValue({
+      id,
+      name,
+      email
+    })
+  })
+
+  beforeEach(() => {
+    sut = new ResetPasswordService(accountRepo)
+  })
+
+  describe('LoadAccountByResetToken Repository', () => {
+    it('should call LoadAccountByResetToken with correct values', async () => {
+      await sut.perform({ token, password })
+
+      expect(accountRepo.loadByResetToken).toHaveBeenCalledWith({ token: 'any_token' })
+      expect(accountRepo.loadByResetToken).toHaveBeenCalledTimes(1)
+    })
+  })
+})
