9009fe975287049854d778b3fa9ae7cf27bec094
Caio Vieira <contato@caiovieira.com.br>
2022-05-27 17:55:05 -0300

    feat: add domain layer to reset password usecase
    issue: #118

---
+++ src/domain/usecases/account/reset-password.ts
@@ -0,0 +1,14 @@
+import { Account } from '@/domain/models'
+
+export interface ResetPassword {
+  perform: (params: ResetPassword.Params) => Promise<ResetPassword.Result>
+}
+
+export namespace ResetPassword {
+  export type Params = {
+    token: string
+    password: string
+  }
+
+  export type Result = Account | undefined
+}
