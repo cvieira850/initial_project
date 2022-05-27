d162a2eb18841d01910842c5eff6188793946152
Caio Vieira <contato@caiovieira.com.br>
2022-05-27 18:52:37 -0300

    feat: ensure ResetPasswordService call Hash with correct password
    issue: #119

--- src/data/services/account/reset-password/reset-password.ts
+++ src/data/services/account/reset-password/reset-password.ts
@@ -1,13 +1,18 @@
-import { ResetPassword, LoadAccountByResetTokenRepository } from './reset-password-protocols'
+import { ResetPassword, LoadAccountByResetTokenRepository, Hash } from './reset-password-protocols'
 import { InvalidRequestError } from '@/data/errors'
 
 export class ResetPasswordService implements ResetPassword {
-  constructor (private readonly accountRepo: LoadAccountByResetTokenRepository) {}
+  constructor (
+    private readonly accountRepo: LoadAccountByResetTokenRepository,
+    private readonly hash: Hash
+  ) {}
+
   async perform (params: ResetPassword.Params): Promise<ResetPassword.Result> {
     const account = await this.accountRepo.loadByResetToken({ token: params.token })
     if (!account) {
       throw new InvalidRequestError('Account not found')
     }
+    await this.hash.hash({ plaintext: params.password })
     return undefined
   }
 }
