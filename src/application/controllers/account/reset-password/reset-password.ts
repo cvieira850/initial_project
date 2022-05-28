b62d5b4d935d6a2a90ee661fc10e49a7d56e451c
Caio Vieira <contato@caiovieira.com.br>
2022-05-28 18:06:32 -0300

    feat: ensure ResetPasswordController build validators correctly
    issue: #117

--- src/application/controllers/account/reset-password/reset-password.ts
+++ src/application/controllers/account/reset-password/reset-password.ts
@@ -1,8 +1,18 @@
 import { HttpRequest, HttpResponse } from '@/application/helpers'
 import { Controller } from '@/application/controllers'
+import { ValidationBuilder as Builder, Validator } from '@/application/validation'
 
 export class ResetPasswordController extends Controller {
   async perform (httpRequest: HttpRequest): Promise<HttpResponse<any>> {
     throw new Error('Method not implemented.')
+  }
+
+  override buildValidators (httpRequest: HttpRequest): Validator[] {
+    return [
+      ...Builder.of({ value: httpRequest.params.token, fieldName: 'token' }).required().string().build(),
+      ...Builder.of({ value: httpRequest.body.password, fieldName: 'password' }).required().string().build(),
+      ...Builder.of({ value: httpRequest.body.passwordConfirmation, fieldName: 'passwordConfirmation' }).required().string().build(),
+      ...Builder.of({ value: httpRequest.body.password, fieldName: 'passwordConfirmation' }).validateCompareFields(httpRequest.body.passwordConfirmation).build()
+    ]
   }
 }
