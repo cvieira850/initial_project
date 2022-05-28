b392a9e4ef07f8659a99aae0d2dc1977c4e8a6bd
Caio Vieira <contato@caiovieira.com.br>
2022-05-28 18:14:26 -0300

    test: ensure ResetPasswordController call ResetPassword with correct values
    issue: #117

--- tests/application/controllers/account/reset-password/reset-password.spec.ts
+++ tests/application/controllers/account/reset-password/reset-password.spec.ts
@@ -1,20 +1,36 @@
 import { Controller, ResetPasswordController } from '@/application/controllers'
 import { CompareFieldsValidation, RequiredValidator, StringValidator } from '@/application/validation'
+import { ResetPassword } from '@/domain/usecases'
+
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('ResetPasswordController', () => {
   let sut: ResetPasswordController
+  let resetPassword: MockProxy<ResetPassword>
   let token: string
   let password: string
   let passwordConfirmation: string
+  let id: string
+  let name: string
+  let email: string
 
   beforeAll(() => {
     token = 'any_token'
     password = 'any_password'
     passwordConfirmation = 'any_password_confirmation'
+    id = 'any_id'
+    name = 'any_name'
+    email = 'any_email'
+    resetPassword = mock()
+    resetPassword.perform.mockResolvedValue({
+      id,
+      name,
+      email
+    })
   })
 
   beforeEach(() => {
-    sut = new ResetPasswordController()
+    sut = new ResetPasswordController(resetPassword)
   })
 
   it('Should be an instance of Controller', async () => {
@@ -33,5 +49,14 @@ describe('ResetPasswordController', () => {
       new StringValidator(passwordConfirmation, 'passwordConfirmation'),
       new CompareFieldsValidation(password, 'passwordConfirmation', passwordConfirmation)
     ])
+  })
+
+  describe('resetPassword', () => {
+    it('Should call resetPassword with correct values', async () => {
+      await sut.perform({ params: { token }, body: { password, passwordConfirmation } })
+
+      expect(resetPassword.perform).toHaveBeenCalledWith({ token, password })
+      expect(resetPassword.perform).toHaveBeenCalledTimes(1)
+    })
   })
 })
